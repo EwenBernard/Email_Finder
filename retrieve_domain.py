0d0915d66efd162265f604a4b5a7845b87e62be0
NathanMacktravis <95182428+NathanMacktravis@users.noreply.github.com>
2023-12-08 21:30:31 +0100

    update backend, retrieve_domain, db files

--- retrieve_domain.py
+++ retrieve_domain.py
@@ -1,25 +1,4 @@
 import requests
-"""
-def get_email_from_hunter(first_name, last_name, company):
-    url = "https://api.hunter.io/v2/domain-search"
-
-    params = {
-        "company": company,
-        "first_name": first_name,
-        "last_name": last_name,
-        "api_key": "58f5d6980b1a5b9b191d4185525104dc40b8eb57"
-    }
-
-    response = requests.get(url, params=params)
-    data = response.json()
-
-    # Obtenez l'adresse e-mail à partir des résultats de la recherche
-    if data.get("data", {}).get("emails"):
-        return data["data"]["emails"][0]["value"]  # Renvoie la première adresse e-mail trouvée
-    else:
-        return None  # Aucune adresse e-mail trouvée
-"""
-
 
 def get_email_from_hunter(first_name, last_name, company):
     url = "https://api.hunter.io/v2/domain-search"
@@ -33,18 +12,19 @@ def get_email_from_hunter(first_name, last_name, company):
 
     response = requests.get(url, params=params)
     data = response.json()
+    return data 
 
-    # Obtenez l'adresse e-mail à partir des résultats de la recherche
+"""    # Obtenez l'adresse e-mail à partir des résultats de la recherche
     if data.get("data", {}).get("emails"):
         return data["data"]["emails"][0]["value"]  # Renvoie la première adresse e-mail trouvée
     else:
         return None  # Aucune adresse e-mail trouvée
-    
+    """
 
 # Exemple d'utilisation de la fonction
-first_name = "julio"
-last_name = "cagec"
-company = "capgemini"
+first_name = "jean"
+last_name = "soma"
+company = "efrei.fr"
 
 email = get_email_from_hunter(first_name, last_name, company)
 
@@ -79,45 +59,3 @@ else:
 
 
 
-
-
-
-
-
-
-"""import requests
-
-def get_domain_by_company_name(api_key, company_name):
-    url = f"https://company.clearbit.com/v2/companies/find?name={company_name}"
-
-    headers = {
-        "Authorization": f"Bearer {api_key}"
-    }
-
-    try:
-        response = requests.get(url, headers=headers)
-        response.raise_for_status()  # Gère les erreurs HTTP
-        data = response.json()
-
-        # Vérifie si des données d'entreprise ont été trouvées
-        if data.get('id'):
-            domain = data['domain']
-            return domain
-        else:
-            return None
-
-    except requests.exceptions.RequestException as e:
-        print(f"Une erreur s'est produite lors de la requête : {e}")
-        return None
-
-# Remplacez "VOTRE_CLE_API_CLEARBIT" par votre clé API Clearbit
-api_key = "sk_2d6388be3ba6277b96f362027e6f1ac2"
-company_name = "airbnb.com"
-
-domain = get_domain_by_company_name(api_key, company_name)
-
-if domain:
-    print(f"Le domaine de {company_name} est : {domain}")
-else:
-    print(f"Aucun domaine trouvé pour {company_name}")
-"""
