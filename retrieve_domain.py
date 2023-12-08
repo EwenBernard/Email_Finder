748dafd6fb8d95f29c29dd94c0cdfe63524bf9e6
NathanMacktravis <95182428+NathanMacktravis@users.noreply.github.com>
2023-12-08 19:01:24 +0100

    backend and index files updated

--- retrieve_domain.py
+++ retrieve_domain.py
@@ -1,13 +1,13 @@
 import requests
-
-def get_email_from_hunter(first_name, last_name, company, api_key):
+"""
+def get_email_from_hunter(first_name, last_name, company):
     url = "https://api.hunter.io/v2/domain-search"
 
     params = {
         "company": company,
         "first_name": first_name,
         "last_name": last_name,
-        "api_key": api_key
+        "api_key": "58f5d6980b1a5b9b191d4185525104dc40b8eb57"
     }
 
     response = requests.get(url, params=params)
@@ -18,14 +18,35 @@ def get_email_from_hunter(first_name, last_name, company, api_key):
         return data["data"]["emails"][0]["value"]  # Renvoie la première adresse e-mail trouvée
     else:
         return None  # Aucune adresse e-mail trouvée
+"""
+
+
+def get_email_from_hunter(first_name, last_name, company):
+    url = "https://api.hunter.io/v2/domain-search"
+
+    params = {
+        "company": company,
+        "first_name": first_name,
+        "last_name": last_name,
+        "api_key": "58f5d6980b1a5b9b191d4185525104dc40b8eb57"
+    }
+
+    response = requests.get(url, params=params)
+    data = response.json()
+
+    # Obtenez l'adresse e-mail à partir des résultats de la recherche
+    if data.get("data", {}).get("emails"):
+        return data["data"]["emails"][0]["value"]  # Renvoie la première adresse e-mail trouvée
+    else:
+        return None  # Aucune adresse e-mail trouvée
+    
 
 # Exemple d'utilisation de la fonction
 first_name = "julio"
-last_name = "ibc"
+last_name = "cagec"
 company = "capgemini"
-api_key = "58f5d6980b1a5b9b191d4185525104dc40b8eb57"
 
-email = get_email_from_hunter(first_name, last_name, company, api_key)
+email = get_email_from_hunter(first_name, last_name, company)
 
 if email:
     print(f"L'adresse e-mail trouvée est : {email}")
