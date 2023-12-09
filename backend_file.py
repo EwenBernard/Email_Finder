6f9faf35977dce2aed5568407ae53066a81aa3ff
NathanMacktravis <95182428+NathanMacktravis@users.noreply.github.com>
2023-12-09 15:23:18 +0100

    updating retrive_email file
    I updated this function to retrieve informations about matched request user.

--- backend_file.py
+++ backend_file.py
@@ -1,10 +1,6 @@
 from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
-import os
-import requests
-from retrieve_domain import get_email_from_hunter
-from flask import Flask, render_template, request, redirect
-from flask_sqlalchemy import SQLAlchemy
+from retrieve_domain import get_email_from_hunter, match_user_input
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///pythonsqlite.db'
@@ -16,13 +12,11 @@ class User(db.Model):
     last_name = db.Column(db.String(80), nullable=False)
     reconstructed_email = db.Column(db.String(120))
     company_name = db.Column(db.String(120))
-
-
+    
 @app.route('/')
 def index():
     users = User.query.all()
     return render_template('index.html', users=users)
-
 
 @app.route('/add', methods=['GET', 'POST'])
 def add_user():
@@ -31,21 +25,26 @@ def add_user():
         last_name = request.form['last_name']
         company_name = request.form['company_name']
 
-        # Utilisez la nouvelle fonction pour obtenir l'e-mail
-        reconstructed_email = get_email_from_hunter(name, last_name, company_name)
+        # Utilisez la nouvelle fonction pour obtenir l'e-mail et le reste des données
+        hunter_data = get_email_from_hunter(name, last_name, company_name)
+        matched_result, remaining_data = match_user_input(f"{name} {last_name}", hunter_data)
 
-        new_user = User(name=name, last_name=last_name, reconstructed_email=reconstructed_email, company_name=company_name)
-        db.session.add(new_user)
-        db.session.commit()
+        if matched_result:
+            new_user = User(name=matched_result['name'],
+                            last_name=matched_result['last_name'],
+                            reconstructed_email=matched_result['email'],
+                            company_name=matched_result['company'])
+            db.session.add(new_user)
+            db.session.commit()
+
         return redirect('/')
-    return render_template('add_user.html')  # Créez un template HTML pour le formulaire d'ajout
 
+    return render_template('add_user.html')
 
 @app.route('/update/<int:user_id>', methods=['POST'])
 def update_user(user_id):
     user = User.query.get(user_id)
     
-    # Mettez à jour uniquement les champs non vides fournis dans la requête
     if 'name' in request.form:
         user.name = request.form['name']
     if 'last_name' in request.form:
@@ -56,7 +55,6 @@ def update_user(user_id):
     db.session.commit()
     return redirect('/')
 
-
 @app.route('/delete/<int:user_id>', methods=['POST'])
 def delete_user(user_id):
     user = User.query.get(user_id)
@@ -64,36 +62,8 @@ def delete_user(user_id):
     db.session.commit()
     return redirect('/')
 
-
-
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-        print("PRINT DATABASE")
+        print("Executing")
         app.run(debug=True)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
